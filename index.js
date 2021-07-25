80c07e2dc1434de734ad70a5226d2142c378a76f
Jogu <jogupersonnel@gmail.com>
2021-07-25 11:26:56 -0700

    second commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let products = document.querySelector('.products');
+let innerHTML = '';
+
+
+for (let i = 0; i < product_name.length; i++) {
+    innerHTML = `
+        <div class="card">
+            <div class="images">
+            <h2 class="product-name">${product_name[i].name}</h2>
+                <img class="large-photo" src="${product_name[i].img_url1}" alt="${product_name[i].name}">
+                <div class="small-photo">
+                    <img class="product_sm" src="${product_name[i].img_url2}" alt="${product_name[i].name}">
+                    <img class="product_sm" src="${product_name[i].img_url3}" alt="${product_name[i].name}">
+                    <img class="product_sm" src="${product_name[i].img_url4}" alt="${product_name[i].name}">
+                </div>
+            </div>
+            <div class="text">
+                <h3 class="price">${product_name[i].price}</h3>
+                <h4 class="description">${product_name[i].description}</h4>
+                <a href="mailto:jogupersonnel@gmail.com"><h4>email me about this product</h4></a>
+            </div>
+        </div>
+    `;
+    products.innerHTML = products.innerHTML + innerHTML;
+}
+
---
+++ products.js
@@ -0,0 +1,29 @@
+let product_name = [
+    {
+        name: "Alfa Network", 
+        img_url1: "images/for_sale_yes/Alfa_Network/IMG_2876-min.JPG", 
+        img_url2: "images/for_sale_yes/Alfa_Network/IMG_2867-min.JPG", 
+        img_url3: "images/for_sale_yes/Alfa_Network/IMG_2874-min.JPG", 
+        img_url4: "images/for_sale_yes/Alfa_Network/IMG_2871-min.JPG", 
+        price: '$' + 49, 
+        description: "This is a product"
+    },
+    {
+        name: "Canon SL2 w/ Sigma 17-50mm lens", 
+        img_url1: "images/for_sale_yes/Canon_SL2/IMG_0801-min.JPG", 
+        img_url2: "images/for_sale_yes/Canon_SL2/IMG_0803-min.JPG", 
+        img_url3: "images/for_sale_yes/Canon_SL2/IMG_0804-min.JPG", 
+        img_url4: "images/for_sale_yes/Canon_SL2/IMG_0805-min.JPG", 
+        price: '$' + 645, 
+        description: "This is a product"
+    },
+    {
+        name: "Canon SL2", 
+        img_url1: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2994-min.JPG", 
+        img_url2: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2997-min.JPG", 
+        img_url3: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2998-min.JPG", 
+        img_url4: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_3000-min.JPG", 
+        price: '$' + 39, 
+        description: "This is a product"
+    }
+];
