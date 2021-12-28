b69622847d4e397e073b85f39be987e648f3528e
jonisland <jogucareers@gmail.com>
2021-12-27 18:14:14 -0800

    deleted canon sl2

--- products.js
+++ products.js
@@ -1,32 +1,23 @@
 let product_name = [
-    {
-        name: "Alfa Network", 
-        img_url1: "images/for_sale_yes/Alfa_Network/IMG_2876-min.JPG", 
-        img_url2: "images/for_sale_yes/Alfa_Network/IMG_2867-min.JPG", 
-        img_url3: "images/for_sale_yes/Alfa_Network/IMG_2874-min.JPG", 
-        img_url4: "images/for_sale_yes/Alfa_Network/IMG_2871-min.JPG", 
-        price: '$' + 24, 
-        description: "AWUS036NH 802.11 b/g/n Long-Range USB Adapter 5db Antenna. Superior Wi-Fi Performance Professionally Tuned Wireless, Better Range & CoverageIncreased Bandwidth, More Applications Increased Wireless Signal Penetration",
-        used: "used"
-    },
-    {
-        name: "Canon SL2 w/ Sigma 17-50mm lens", 
-        img_url1: "images/for_sale_yes/Canon_SL2/IMG_0801-min.JPG", 
-        img_url2: "images/for_sale_yes/Canon_SL2/IMG_0803-min.JPG", 
-        img_url3: "images/for_sale_yes/Canon_SL2/IMG_0804-min.JPG", 
-        img_url4: "images/for_sale_yes/Canon_SL2/IMG_0805-min.JPG", 
-        price: '$' + 645, 
-        description: "Lightweight, fun and easy to use, the Canon EOS Rebel SL2 camera further proves that quality is key, helping you capture stunning photos and videos you'll love to see and share. The 24.2 Megapixel CMOS (APS-C) sensor and DIGIC 7 Image Processor",
-        used: "used"
-    },
-    {
-        name: "GH Bass & Co Black Shoes", 
-        img_url1: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2994-min.JPG", 
-        img_url2: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2997-min.JPG", 
-        img_url3: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2998-min.JPG", 
-        img_url4: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_3000-min.JPG", 
-        price: '$' + 39, 
-        description: "GH Bass & Co Black Shoes",
-        used: "used"
-    }
+  {
+    name: "Alfa Network",
+    img_url1: "images/for_sale_yes/Alfa_Network/IMG_2876-min.JPG",
+    img_url2: "images/for_sale_yes/Alfa_Network/IMG_2867-min.JPG",
+    img_url3: "images/for_sale_yes/Alfa_Network/IMG_2874-min.JPG",
+    img_url4: "images/for_sale_yes/Alfa_Network/IMG_2871-min.JPG",
+    price: "$" + 24,
+    description:
+      "AWUS036NH 802.11 b/g/n Long-Range USB Adapter 5db Antenna. Superior Wi-Fi Performance Professionally Tuned Wireless, Better Range & CoverageIncreased Bandwidth, More Applications Increased Wireless Signal Penetration",
+    used: "used",
+  },
+  {
+    name: "GH Bass & Co Black Shoes",
+    img_url1: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2994-min.JPG",
+    img_url2: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2997-min.JPG",
+    img_url3: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_2998-min.JPG",
+    img_url4: "images/for_sale_yes/GH_Bass&Co_Black_Shoes/IMG_3000-min.JPG",
+    price: "$" + 39,
+    description: "GH Bass & Co Black Shoes",
+    used: "used",
+  },
 ];
